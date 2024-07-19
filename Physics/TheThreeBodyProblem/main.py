2a0d52cf5a1290b08f9eca822d6a13211531992a
Ansh Lohani <lohaniansh23@gmail.com>
2024-07-19 22:50:13 +0530

    add simulation dictionary and define turtles

--- Physics/TheThreeBodyProblem/main.py
+++ Physics/TheThreeBodyProblem/main.py
@@ -1,47 +1,47 @@
 import turtle
 import random 
 
-#Defining Initial Conditions of Bodies a, b, c.
-class initial_conditions:
-    #Defining Initial Locations 
-    def location_a(a):
-        '''
-        a --> list()
-        a = [x1,y1] --> 2D cordinates of body "a"
+
+
+#Defining Initial Locations 
+def location_a(a):
+    '''
+    a --> list()
+    a = [x1,y1] --> 2D cordinates of body "a"
         
-        returns a vector 'va' } nested list
-        Format: va = [ [ax,i] [ay,j] ]
-        ''' 
-        va = [[a[0],"i"],[a[1],"j"]]
-        return va
+    returns a vector 'va' } nested list
+    Format: va = [ [ax,i] [ay,j] ]
+    ''' 
+    va = [[a[0],"i"],[a[1],"j"]]
+    return va
         
 
-    def location_b(b):
-        '''
-        b --> list()
-        b = [x2,y2] --> 2D cordinates of body "b"
-        returns a vector 'vb' } nested list
-        Format: vb = [ [bx,i] [by,j] ]
-        ''' 
-        vb = [[b[0],"i"],[b[1],"j"]]
-        return vb
+def location_b(b):
+    '''
+    b --> list()
+    b = [x2,y2] --> 2D cordinates of body "b"
+    returns a vector 'vb' } nested list
+    Format: vb = [ [bx,i] [by,j] ]
+    ''' 
+    vb = [[b[0],"i"],[b[1],"j"]]
+    return vb
 
-    def location_c(c):
-        '''
-        c --> list()
-        c = [x3,y3] --> 2D cordinates of body "c"
-        returns a vector 'vc' } nested list
-        Format: cb = [ [cx,i] [cy,j] ]
-        ''' 
-        vc = [[c[0],"i"],[c[1],"j"]]
-        return vc
+def location_c(c):
+    '''
+    c --> list()
+    c = [x3,y3] --> 2D cordinates of body "c"
+    returns a vector 'vc' } nested list
+    Format: cb = [ [cx,i] [cy,j] ]
+    ''' 
+    vc = [[c[0],"i"],[c[1],"j"]]
+    return vc
     
-    def vel_a():
+def vel_a():
         ...
 
-    def vel_b():
+def vel_b():
         ...
-    def vel_c():
+def vel_c():
         ...
 
 
@@ -51,39 +51,54 @@ screen.bgcolor("black")
 screen.setup(width=1080, height=720)
 
 
-# Body definitions (mass, radius, color)
-bodies = [
-    [10, 20],
-    [15, 20],
-    [20, 20],
-]
 
-# Create the turtles for each body
-turtles = []
-for body in bodies:
-    t = turtle.Turtle()
-    t.shape("circle")
-    t.shapesize(body[1] / 10)  # Scale circle size based on radius
-    t.color("white")
-    turtles.append(t)
 
-# Random starting positions (within screen boundaries)
-for t in turtles:
-    t.penup()
-    x = random.randint(-screen.window_width() // 2 + body[1], screen.window_width() // 2 - body[1])
-    y = random.randint(-screen.window_height() // 2 + body[1], screen.window_height() // 2 - body[1])
-#    z = random.randint(-screen.window_height() // 2 + body[1], screen.window_height() // 2 - body[1])
-    t.setx(x)
-    t.sety(y)
-    t.pendown()
+
+
+#initial cordinates for the 3 bodies
+initial_cordinates = []
+while True:
+    x=random.randint(-screen.window_width() // 2 + 20, screen.window_width() // 2 - 20)
+    y=random.randint(-screen.window_height() // 2 + 20, screen.window_height() // 2 - 20)
+    c = [x,y]
+    if c not in initial_cordinates:
+        initial_cordinates.append(c)
+        if len(initial_cordinates)==3:
+            break
+        else:
+            continue     
+    else:
+        pass
+
+
+
+'''
+simulation = {t1:[x,y],...}
+#t1 = [mass="10"]
+'''
+
+
+t1=turtle.Turtle(shape="circle")
+t2=turtle.Turtle(shape="circle")
+t3=turtle.Turtle(shape="circle")
+bodies = [t1,t2,t3]
+for i in bodies:
+    i.shapesize(10)
+    i.color("white")
+
+
+simulation = dict()
+simulation[t1]=[initial_cordinates[0][0],initial_cordinates[0][1]]
+simulation[t2]=[initial_cordinates[1][0],initial_cordinates[1][1]]
+simulation[t3]=[initial_cordinates[2][0],initial_cordinates[2][1]]
+
+
+print(simulation)
+
 
 
 while True:
-    for t in turtles:
-        # Random change in direction
-        t.setheading(random.randint(0, 360))
-        # Move forward based on mass (larger = slower)
-        t.forward(10)
+    la = location_a([t1.xcor(),t1.ycor()])
 
     screen.update()
 
