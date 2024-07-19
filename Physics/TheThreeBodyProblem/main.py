1ea36783c6187d875f2b64f59ebdc8793d64f5b2
Ansh Lohani <lohaniansh23@gmail.com>
2024-07-19 22:19:22 +0530

    change form 3D to 2D

--- Physics/TheThreeBodyProblem/main.py
+++ Physics/TheThreeBodyProblem/main.py
@@ -7,33 +7,33 @@ class initial_conditions:
     def location_a(a):
         '''
         a --> list()
-        a = [x1,y1,z1] --> 3D cordinates of body "a"
+        a = [x1,y1] --> 2D cordinates of body "a"
         
         returns a vector 'va' } nested list
-        Format: va = [ [ax,i] [ay,j] [az,k] ]
+        Format: va = [ [ax,i] [ay,j] ]
         ''' 
-        va = [[a[0],"i"],[a[1],"j"],[a[2],"k"]]
+        va = [[a[0],"i"],[a[1],"j"]]
         return va
         
 
     def location_b(b):
         '''
         b --> list()
-        b = [x2,y2,z2] --> 3D cordinates of body "b"
+        b = [x2,y2] --> 2D cordinates of body "b"
         returns a vector 'vb' } nested list
-        Format: vb = [ [bx,i] [by,j] [bz,k] ]
+        Format: vb = [ [bx,i] [by,j] ]
         ''' 
-        vb = [[b[0],"i"],[b[1],"j"],[b[2],"k"]]
+        vb = [[b[0],"i"],[b[1],"j"]]
         return vb
 
     def location_c(c):
         '''
         c --> list()
-        c = [x3,y3,z3] --> 3D cordinates of body "c"
+        c = [x3,y3] --> 2D cordinates of body "c"
         returns a vector 'vc' } nested list
-        Format: cb = [ [cx,i] [cy,j] [cz,k] ]
+        Format: cb = [ [cx,i] [cy,j] ]
         ''' 
-        vc = [[c[0],"i"],[c[1],"j"],[c[2],"k"]]
+        vc = [[c[0],"i"],[c[1],"j"]]
         return vc
     
     def vel_a():
@@ -76,7 +76,8 @@ for t in turtles:
     t.setx(x)
     t.sety(y)
     t.pendown()
-    
+
+
 while True:
     for t in turtles:
         # Random change in direction
